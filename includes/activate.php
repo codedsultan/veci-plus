a29c951223088abd256cf1203549d7f4ecf24144
codesultan <codesultan369@gmail.com>
2024-07-19 20:15:36 +0100

    flush-rewrite_rules

--- includes/activate.php
+++ includes/activate.php
@@ -6,4 +6,8 @@ function vp_activate_plugin() {
     {
         wp_die(__('You must update WordPress to use this plugin', 'veci-plus'));
     }
+
+    vp_recipe_post_type();
+
+    flush_rewrite_rules(); //to create rewrite rule for new custom post typ
 }
