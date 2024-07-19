35f742274ec270db34de7b31b27e963e24cf603e
codesultan <codesultan369@gmail.com>
2024-07-19 17:51:21 +0100

    submit-login

--- includes/front/enqueue.php
+++ includes/front/enqueue.php
@@ -2,7 +2,8 @@
 
 function vp_enqueue_scripts() {
     $authURLs = json_encode([
-        'signup' => esc_url_raw(rest_url('vp/v1/signup'))
+        'signup' => esc_url_raw(rest_url('vp/v1/signup')),
+        'signin' => esc_url_raw(rest_url('vp/v1/signin'))
     ]);
 
     wp_add_inline_script( 
