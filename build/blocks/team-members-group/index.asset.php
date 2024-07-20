6be2dbb3ee47720617036ca08724dada3bb015bd
codesultan <codesultan369@gmail.com>
2024-07-20 14:13:02 +0100

    inner-block-support

---
+++ build/blocks/team-members-group/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '79e8aead1180d2b0d1c3');
---
+++ src/blocks/team-members-group/index.js
@@ -0,0 +1,61 @@
+import { registerBlockType } from '@wordpress/blocks';
+import { 
+  useBlockProps, InspectorControls ,InnerBlocks
+} from '@wordpress/block-editor';
+import {
+  PanelBody, RangeControl, SelectControl
+} from '@wordpress/components'
+import { __ } from '@wordpress/i18n';
+import icons from '../../icons.js';
+import './main.css';
+
+registerBlockType('veci-plus/team-members-group', {
+  icon: {
+    src: icons.primary
+  },
+  edit({ attributes, setAttributes }) {
+    const { columns, imageShape } = attributes;
+    const blockProps = useBlockProps({
+      className: "wp-block-udemy-plus-team-members-group"
+    });
+   
+    return (
+      <>
+        <InspectorControls>
+          <PanelBody title={__('Settings', 'veci-plus')}>
+            <RangeControl 
+              label={__('Columns', 'veci-plus')}
+              onChange={columns => setAttributes({columns})}
+              value={columns}
+            />
+            <SelectControl 
+              label={__('Image Shape', 'veci-plus')}
+              value={ imageShape }
+              options={[
+                  { label: __('Hexagon', 'veci-plus'), value: 'hexagon' },
+                  { label: __('Rabbet', 'veci-plus'), value: 'rabbet' },
+                  { label: __('Pentagon', 'veci-plus'), value: 'pentagon' },
+              ]}
+              onChange={imageShape => setAttributes({ imageShape })}
+            />
+          </PanelBody>
+        </InspectorControls>
+        <div {...blockProps}>
+          <InnerBlocks
+            orientation='horizontal'
+            allowedBlocks={['veci-plus/team-member']}
+          />
+        </div>
+      </>
+    );
+  },
+  save({ attributes }) {
+    const blockProps = useBlockProps.save();
+
+    return (
+      <div {...blockProps}>
+          <InnerBlocks.Content/>
+      </div>
+    )
+  }
+});
