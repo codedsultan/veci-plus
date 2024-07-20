c884ec0ef224202ee2f70a9daa0541135125aff7
codesultan <codesultan369@gmail.com>
2024-07-20 10:31:26 +0100

    create-db-table

--- includes/activate.php
+++ includes/activate.php
@@ -9,5 +9,20 @@ function vp_activate_plugin() {
 
     vp_recipe_post_type();
 
-    flush_rewrite_rules(); //to create rewrite rule for new custom post typ
+    flush_rewrite_rules(); //to create rewrite rule for new custom post type
+
+    global $wpdb;
+    $tableName = "{$wpdb->prefix}recipe_rating";
+    $charsetCollate = $wpdb->get_charset_collate();
+    $sql = "
+    CREATE TABLE $tableName (
+        ID bigint(20) unsigned NOT NULL AUTO_INCREMENT PRIMARY KEY,
+        post_id bigint(20) unsigned NOT NULL,
+        user_id bigint(20) unsigned NOT NULL,
+        rating float(3,2) unsigned NOT NULL
+    )   ENGINE='InnoDB' {$charsetCollate};
+    ";
+
+    require_once(ABSPATH. "/wp-admin/includes/upgrade.php");
+    dbDelta($sql);
 }
