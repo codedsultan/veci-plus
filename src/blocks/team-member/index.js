b780b10d2d461d4d7757defda9d0a10e46f570e3
codesultan <codesultan369@gmail.com>
2024-07-20 16:53:59 +0100

    handle-blob

--- src/blocks/team-member/index.js
+++ src/blocks/team-member/index.js
@@ -6,6 +6,7 @@ import { __ } from '@wordpress/i18n';
 import { 
   PanelBody, TextareaControl
 } from '@wordpress/components';
+import {isBlobURL} from '@wordpress/blob';
 import icons from '../../icons.js';
 import './main.css';
 
@@ -36,15 +37,37 @@ registerBlockType('veci-plus/team-member', {
         </InspectorControls>
         <div {...blockProps}>
           <div className="author-meta">
-            <img /> 
+            {
+              imgURL && <img src={imgURL} alt={imgAlt} /> 
+            }
             <MediaPlaceholder 
               acceptedTyoes={['image']}  // for specific 'image/png'
               accept={'image/*'} // for upoads
               icon="admin-users" //placeholder
               onSelect={ img => {
-                console.log(img)
+
+                let newImgURL = null
+                if(isBlobURL(img.url)){
+                  newImgURL = img.url
+                }else{
+                  newImgURL = img.sizes ? img.sizes.teamMember.url :img.media_details.sizes.teamMember.source_url
+                }
+                setAttributes({
+                  imgID: img.id,
+                  imgAlt: img.alt,
+                  imgURL: newImgURL
+                })
+                // console.log(img)
               }}
               onError={error => console.error(TypeError)}
+              disableMediaButtons={imgURL}
+              onSelectURL={url => {
+                setAttributes({
+                  imgID: null,
+                  imgAlt: null,
+                  imgURL: url
+                })
+              }}
             />
             <p>
               <RichText 
@@ -83,7 +106,9 @@ registerBlockType('veci-plus/team-member', {
     return (
       <div {...blockProps}>
         <div class="author-meta">
-          <img />
+            {
+              imgURL && <img src={imgURL} alt={imgAlt} /> 
+            }
           <p>
             <RichText.Content tagName="strong" value={name} />
             <RichText.Content tagName="span" value={title} />
