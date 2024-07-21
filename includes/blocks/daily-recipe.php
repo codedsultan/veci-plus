15f3a8a48482987ae5822a9257d47e1aeae44994
codesultan <codesultan369@gmail.com>
2024-07-21 22:32:13 +0100

    perform-check

--- includes/blocks/daily-recipe.php
+++ includes/blocks/daily-recipe.php
@@ -8,6 +8,29 @@ function vp_daily_recipe_cb($atts){
     if(!$id) {
         $id = vp_generate_daily_recipe();
     }
+    // performance test
+    // global $wpdb;
+    // $startTime = microtime(true);
+    // $id = $wpdb->get_var(
+    //     "SELECT ID from {$wpdb->posts} 
+    //     WHERE post_status='publish' AND post_type='recipe'
+    //     ORDER BY rand() LIMIT 1"
+    // );
+    // $endTime = microtime(true);
+    // $SQLExecutionTime = $endTime - $startTime;
+
+    // $startTime = microtime(true);
+    // new WP_Query([
+    //     'post_type' => 'recipe',
+    //     'post_status' => 'publish',
+    //     'post_per_page' => 1,
+    //     'orderby' => 'rand'
+    // ]);
+    // $endTime = microtime(true);
+    // $WPExecutionTime = $endTime - $startTime;
+    // SQL execution Time : 0.00098991394042969
+    // WP Execuion Time :0.0035479068756104
+    // end performance test
     ob_start();
     ?>
         <div class="wp-block-veci-plus-daily-recipe">
@@ -16,8 +39,6 @@ function vp_daily_recipe_cb($atts){
                 <img src="<?php echo get_the_post_thumbnail_url($id ,"full"); ?>">
                 <h3><?php echo get_the_title($id) ?></h3>
             </a>
-
-
         </div>
     <?php
 
--- includes/generate-daily-recipe.php
+++ includes/generate-daily-recipe.php
@@ -6,11 +6,27 @@ function vp_generate_daily_recipe(){
 
     // no ned to prepare because it has no user input
     // query dynamic post($wpdb->posts) table name to avoid issues
+    // $startTime = microtime(true);
     $id = $wpdb->get_var(
         "SELECT ID from {$wpdb->posts} 
         WHERE post_status='publish' AND post_type='recipe'
         ORDER BY rand() LIMIT 1"
     );
+    // $endTime = microtime(true);
+    // $SQLExecutionTime = $endTime - $startTime;
+
+    // $startTime = microtime(true);
+    // new WP_Query([
+    //     'post_type' => 'recipe',
+    //     'post_status' => 'publish',
+    //     'post_per_page' => 1,
+    //     'orderby' => 'rand'
+    // ]);
+    // $endTime = microtime(true);
+    // $WPExecutionTime = $endTime - $startTime;
+    // SQL execution Time : 0.00098991394042969 // good
+    // WP Execuion Time :0.0035479068756104 // too slow for this operation
+
 
     set_transient('vp_daily_recipe_id',$id ,DAY_IN_SECONDS); // use transient to store id temporarily for a day
 
