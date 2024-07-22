faf821048a30823cc1fa63b995b43ffddff39629
codesultan <codesultan369@gmail.com>
2024-07-22 04:15:00 +0100

    add-new-image-size

--- includes/image-sizes.php
+++ includes/image-sizes.php
@@ -2,6 +2,7 @@
 
 function vp_custom_image_sizes($sizes){
     return array_merge($sizes,[
-        'teamMember' => __('Team Member', 'veci-plus')
+        'teamMember' => __('Team Member', 'veci-plus'),
+        'opengraph' => __('Open Graph', 'veci-plus')
     ]);
 }
--- includes/setup.php
+++ includes/setup.php
@@ -4,4 +4,7 @@ function vp_setup_theme() {
     add_image_size(
         'teamMember' , 56 , 56 , true
     );
+    add_image_size(
+        'opengraph' , 1200 , 630 , true
+    );
 }
