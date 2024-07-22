d4ac08871fc5b790ecfc4c7f4b9df9fd288f62a4
codesultan <codesultan369@gmail.com>
2024-07-22 06:15:44 +0100

    add-ui-for-meta

--- build/block-editor/index.js
+++ build/block-editor/index.js
@@ -12,6 +12,26 @@ module.exports = window["React"];
 
 /***/ }),
 
+/***/ "@wordpress/components":
+/*!************************************!*\
+  !*** external ["wp","components"] ***!
+  \************************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["components"];
+
+/***/ }),
+
+/***/ "@wordpress/data":
+/*!******************************!*\
+  !*** external ["wp","data"] ***!
+  \******************************/
+/***/ ((module) => {
+
+module.exports = window["wp"]["data"];
+
+/***/ }),
+
 /***/ "@wordpress/edit-post":
 /*!**********************************!*\
   !*** external ["wp","editPost"] ***!
@@ -52,18 +72,45 @@ __webpack_require__.r(__webpack_exports__);
 /* harmony import */ var react__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! react */ "react");
 /* harmony import */ var _wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! @wordpress/plugins */ "@wordpress/plugins");
 /* harmony import */ var _wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! @wordpress/edit-post */ "@wordpress/edit-post");
-/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_components__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! @wordpress/components */ "@wordpress/components");
+/* harmony import */ var _wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! @wordpress/i18n */ "@wordpress/i18n");
+/* harmony import */ var _wordpress_data__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! @wordpress/data */ "@wordpress/data");
+
+
 
 
 
 
 (0,_wordpress_plugins__WEBPACK_IMPORTED_MODULE_1__.registerPlugin)("vp-sidebar", {
   render() {
+    const {
+      og_title,
+      og_image,
+      og_description,
+      og_override_image
+    } = (0,_wordpress_data__WEBPACK_IMPORTED_MODULE_5__.useSelect)(select => {
+      return select('core/editor').getEditedPostAttribute("meta");
+    });
     return (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_edit_post__WEBPACK_IMPORTED_MODULE_2__.PluginSidebar, {
       name: "vp_sidebar",
       icon: "share",
-      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_3__.__)("Veci Plus Sidebar", "veci-plus")
-    }, "Random Text");
+      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Veci Plus Sidebar", "veci-plus")
+    }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.PanelBody, {
+      title: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Opengraph Options", "veci-plus")
+    }, (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Title", "veci-plus"),
+      value: og_title,
+      onChange: og_title => {}
+    }), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.TextareaControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Description", "veci-plus"),
+      value: og_description,
+      onChange: og_description => {}
+    }), (0,react__WEBPACK_IMPORTED_MODULE_0__.createElement)(_wordpress_components__WEBPACK_IMPORTED_MODULE_3__.ToggleControl, {
+      label: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("Override Featured Image", "veci-plus"),
+      checked: og_override_image,
+      help: (0,_wordpress_i18n__WEBPACK_IMPORTED_MODULE_4__.__)("By default, the featured image will be used as the image. Check this option to use a different image.", "veci-plus"),
+      onChange: og_override_image => {}
+    })));
   }
 });
 
--- src/block-editor/sidebar.js
+++ src/block-editor/sidebar.js
@@ -1,14 +1,46 @@
 import {registerPlugin} from "@wordpress/plugins"
 import {PluginSidebar} from "@wordpress/edit-post"
-import { __ } from "@wordpress/i18n"
+import {PanelBody,TextControl,TextareaControl,ToggleControl} from "@wordpress/components"
+import { __ } from "@wordpress/i18n";
+import {useSelect} from "@wordpress/data";
 registerPlugin("vp-sidebar", {
     render() {
+
+        const {og_title, og_image, og_description, og_override_image} = useSelect((select)=> {
+            return select('core/editor').getEditedPostAttribute("meta")
+        })
         return <PluginSidebar
         name="vp_sidebar"
         icon="share"
         title={__("Veci Plus Sidebar", "veci-plus")}
         >
-            Random Text
+            <PanelBody title={__("Opengraph Options", "veci-plus")}>
+            <TextControl 
+                label={__("Title", "veci-plus")}
+                value={og_title}
+                onChange={og_title => {
+
+                }}
+            />
+            <TextareaControl 
+                label={__("Description", "veci-plus")}
+                value={og_description}
+                onChange={og_description => {
+
+                }}
+            />
+            <ToggleControl 
+                label={__("Override Featured Image", "veci-plus")}
+                checked={og_override_image}
+                help={__(
+                "By default, the featured image will be used as the image. Check this option to use a different image.",
+                "veci-plus"
+                )}
+                onChange={og_override_image => {
+
+                }}
+            />
+            </PanelBody>
         </PluginSidebar>
     }
 })
