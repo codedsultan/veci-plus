2440815f66792f7201e3f25b2320766f6a72cdf6
codesultan <codesultan369@gmail.com>
2024-07-22 05:17:51 +0100

    alt-settings-api-page

--- includes/admin/enqueue.php
+++ includes/admin/enqueue.php
@@ -2,7 +2,10 @@
 
 function vp_admin_enqueue($hook_suffix){
    // echo $hook_suffix; // get plugin option page suffix
-   if($hook_suffix === "toplevel_page_vp-plugin-options"){
+   if(
+    $hook_suffix === "toplevel_page_vp-plugin-options" ||
+    $hook_suffix === "veci-plus_page_vp-plugin-options-alt"
+    ){
         wp_enqueue_media();
         wp_enqueue_style('vp_admin');
         wp_enqueue_script('vp_admin');
--- includes/admin/options-page-alt.php
+++ includes/admin/options-page-alt.php
@@ -3,7 +3,13 @@
 function vp_plugin_options_alt_page () {
     ?>
         <div class="wrap">
-            Hello
+            <form method="POST" action="options.php">
+                <?php 
+                    settings_fields('vp_options_group');
+                    do_settings_sections('vp-options-page');
+                    submit_button();
+                 ?>
+            </form>
         </div>
     <?php
 }
